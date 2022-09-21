d04a4e4fa853c5ba39dcaa9c66a24892b525e335
Ishu-sri-001 <104553220+Ishu-sri-001@users.noreply.github.com>
2022-09-21 02:40:30 -0700

    Create helloworld.cpp

---
+++ assignment 1/helloworld.cpp
@@ -0,0 +1,15 @@
+//https://www.hackerrank.com/challenges/cpp-input-and-output/problem?isFullScreen=true
+
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int a,b,c;
+    cin>>a>>b>>c;
+    int sum= a+b+c;
+    cout<<sum<<"\n";
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
