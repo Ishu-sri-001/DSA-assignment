2b2688c8c23e06c4455734d63b130f5294ee9bf8
Ishu-sri-001 <104553220+Ishu-sri-001@users.noreply.github.com>
2022-09-24 19:29:31 +0530

    Create HelloWorld.c

---
+++ assignment 2/HelloWorld.c
@@ -0,0 +1,11 @@
+//https://www.hackerrank.com/challenges/hello-world-c/problem?isFullScreen=true
+#include <stdio.h>
+int main() 
+{
+	printf("Hello, World! \n");
+    char s[100];
+    fgets(s, 100, stdin);
+  	printf("%s" ,s);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
