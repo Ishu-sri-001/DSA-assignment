50e36ff0ed2cf496daaddb242de926f19c970ec5
Ishita Srivastav <104553220+Ishu-sri-001@users.noreply.github.com>
2022-10-05 18:36:29 +0530

    Create simplearraysum.cpp

---
+++ assignment 3/simplearraysum.cpp
@@ -0,0 +1,20 @@
+//https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int l;
+    cin>>l;
+    int arr[l];
+    for (int i=0;i<l;i++)
+    {
+        cin>>arr[i];
+    }
+    int sum=0;
+    for (int i=0;i<l;i++)
+    {
+        sum=sum+arr[i];
+    }
+    cout<<sum;
+    return 0;
+}
