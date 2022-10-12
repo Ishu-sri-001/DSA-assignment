7d6fbe408e4a75acf4d058eab2a4a6eb197fb085
Ishita Srivastav <104553220+Ishu-sri-001@users.noreply.github.com>
2022-10-12 15:02:33 +0530

    Create breakingRecords.cpp

---
+++ Assignment 4 (Implementation)/breakingRecords.cpp
@@ -0,0 +1,30 @@
+//https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem?isFullScreen=true
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int l;
+    cin>>l;
+    int arr[l];
+    int win=0,loss=0;
+    for (int i=0;i<l;i++)
+    {
+        cin>>arr[i];
+    }
+    int min =arr[0]; 
+    int max=0;   
+    for (int i=0;i<l;i++)
+    {
+        if (arr[i]>max)
+        {
+        max=arr[i];
+        win++;
+        }
+        if (min>arr[i])
+        {
+            min=arr[i];
+            loss++;
+        }
+    }
+    cout<<win<<" "<<loss;
+}
