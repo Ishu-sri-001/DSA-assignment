8e5d47b0dfd4b8c9655db098210baf1cab683ae5
Ishita Srivastav <104553220+Ishu-sri-001@users.noreply.github.com>
2022-12-26 10:53:23 +0530

    Update FirstNdLstIndex.cpp

--- Binary Search/FirstNdLstIndex.cpp
+++ Binary Search/FirstNdLstIndex.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         int l=nums.size();
-        int a[2];
-        a[0]=-1;
-        a[1]=-1;
-        int f=-1;
-        int lst=-1;
+        vector<int> a={-1,-1};
+        
+        
         for (int i=0;i<l;i++)
         {
-            if (nums[i]==target)
-            f=i;
+            if (nums[i]==target){
+            a[0]=i;
             break;
+            }
         }
-        for(int j=l;j>=0;j--)
+        for(int j=l-1;j>=0;j--)
         {
-            if (nums[j]==target)
-            lst=j;
+            if (nums[j]==target){
+            a[1]=j;
             break;
+            }
         }
-        a[0]=f;
-        a[1]=lst;
-        return a[];
+       // cout<<"["<<f<<","<<lst<<"]";
+       // return a;
+        return a;
     }
 };
